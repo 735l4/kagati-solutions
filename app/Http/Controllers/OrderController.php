53cfbe464e1d6a49282f29d96771e559dfe23234
Pratibimba Khadka <pratibimbakhadka@gmail.com>
2021-06-20 16:30:20 +0545

    Added currency conversion support

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,20 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use App\Models\User;
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class OrderController extends Controller
 {
-    public function complete_order($id) {
+    public function complete_order($id, Request $request) {
+        $cur = $request->input("cur", "USD");
+        $order = Order::findOrFail($id);
 
-       $order = Order::findOrFail($id);
+        if($cur != "USD") {
+            $conv_id = "{$cur}_USD";
+            $string = Http::get("https://free.currencyconverterapi.com/api/v6/convert?q=$conv_id&compact=ultra&apiKey=10e51f9636615fbe228b");
+            $curr_val = json_decode($string, true);
+            $total_amount = round($order->total_amount * $curr_val[$conv_id], 2);
+        } 
+
+        else {
+            $total_amount = $order->total_amount;
+        }
+
+
        if($order->order_status != 'Complete')  {
             $order->order_status = 'Complete';
             $order->save();
             $order->rewards()->create([
-                'points' => $order->total_amount,
+                'points' => $total_amount,
                 'user_id' => 1,
                 'expires_on' => date('Y-m-d', strtotime('+1 year')) 
             ]);
